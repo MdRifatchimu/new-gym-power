60ea7b4a9daa9577c54aa010ba832cb0ee651890
MdRifatchimu <himu2411@swfu.edu.cn>
2021-10-18 07:28:06 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBIyujgu3pYyGWTs58s5QtftUtmBYgTa98",
+
+  authDomain: "new-gym-power.firebaseapp.com",
+
+  projectId: "new-gym-power",
+
+  storageBucket: "new-gym-power.appspot.com",
+
+  messagingSenderId: "768576649484",
+
+  appId: "1:768576649484:web:cafa09aeeda1f0bed87129"
+};
+
+export default firebaseConfig;
